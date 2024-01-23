3acd151f81592c915ab78e61c3f4039bbc0dabc0
OllyYa <exrhodo@gmail.com>
2024-01-23 12:32:07 +0300

    fe lead view

--- frontend/ollycrm_vue/src/router/index.js
+++ frontend/ollycrm_vue/src/router/index.js
@@ -8,6 +8,7 @@ import LogIn from '../views/LogIn.vue'
 import Dashboard from '../views/dashboard/Dashboard.vue'
 import MyAccount from '../views/dashboard/MyAccount.vue'
 import Leads from '../views/dashboard/Leads.vue'
+import Lead from '../views/dashboard/Lead.vue'
 import AddLead from '../views/dashboard/AddLead.vue'
 
 const routes = [
@@ -62,6 +63,14 @@ const routes = [
     path: '/dashboard/leads/add',
     name: 'AddLead',
     component: AddLead,
+    meta: {
+      requiredLogin: true
+    }
+  },
+  {
+    path: '/dashboard/leads/:id',
+    name: 'Lead',
+    component: Lead,
     meta: {
       requiredLogin: true
     }
