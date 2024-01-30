95028b8e5e221e4bb4237bd35ff28c6c883ab6bc
OllyYa <exrhodo@gmail.com>
2024-01-30 12:29:22 +0300

    note serializer and be-view

--- backend/client/models.py
+++ backend/client/models.py
@@ -22,3 +22,6 @@ class Note(models.Model):
     client = models.ForeignKey(Client, related_name='notes', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     body = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
